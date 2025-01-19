1a3fb2b0cceb37938d9adf637900d9fa51165a5a
kuliginproject <kuligin.e.s@gmail.com>
2025-01-19 17:57:16 +0300

    Add files via upload

---
+++ precache-manifest.63ff752f028f5c5b615638af3e443929.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e78476e54720d7c87813581f6aace257",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5a85787208c04bf7aa6c",
+    "url": "/static/css/main.a9089c74.chunk.css"
+  },
+  {
+    "revision": "e266eec883dfbc0ffb9a",
+    "url": "/static/js/2.1a240082.chunk.js"
+  },
+  {
+    "revision": "5a85787208c04bf7aa6c",
+    "url": "/static/js/main.04de9586.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
